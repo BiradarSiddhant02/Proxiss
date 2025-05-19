d8f8b0bebfcb1b50d6814b9ed54b94cdfca50361
BiradarSiddhant02 <pes1202102033@pesu.ped.edu>
2025-05-19 05:53:27 +0530

    add inference time display

--- examples/inference.py
+++ examples/inference.py
@@ -4,10 +4,24 @@ import faiss
 from proxi import ProxiFlat
 from tabulate import tabulate
 from sentence_transformers import SentenceTransformer
+from time import perf_counter_ns
+
+
+def print_time(time):
+    if time > 1e3:
+        return f"{time / 1e3} us"
+    elif time > 1e6:
+        return f"{time / 1e6} ms"
+    elif time > 1e9:
+        return f"{time / 1e9} s"
+    else:
+        return f"{time} ns"
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Compare Proxi and FAISS similarity search.")
+    parser = argparse.ArgumentParser(
+        description="Compare Proxi and FAISS similarity search."
+    )
     parser.add_argument(
         "--embeddings",
         type=str,
@@ -20,7 +34,9 @@ def main():
         required=True,
         help="Path to the .npy file with text entries.",
     )
-    parser.add_argument("-k", type=int, required=True, help="Number of neighbors to retrieve.")
+    parser.add_argument(
+        "-k", type=int, required=True, help="Number of neighbors to retrieve."
+    )
     args = parser.parse_args()
 
     k = args.k
@@ -58,10 +74,14 @@ def main():
         input_embedding = model.encode([user_input], convert_to_numpy=True)
         input_embedding_reshaped = input_embedding.reshape(1, -1).astype(np.float32)
 
+        proxi_start = perf_counter_ns()
         proxi_results = proxi_index.find_docs(input_embedding_reshaped[0])
+        proxi_end = perf_counter_ns()
 
+        faiss_start = perf_counter_ns()
         _, faiss_indices = faiss_index.search(input_embedding_reshaped, k)
         faiss_results = [text[idx] for idx in faiss_indices[0]]
+        faiss_end = perf_counter_ns()
 
         print(f"\nSimilarity search results for: '{user_input}'")
 
@@ -71,6 +91,8 @@ def main():
 
         headers = ["Rank", "Proxi Results", "FAISS Results"]
         print(tabulate(table_data, headers=headers, tablefmt="grid"))
+        print(f"ProxiFlat time: {print_time(proxi_end - proxi_start)}")
+        print(f"FAISS time    : {print_time(faiss_end - faiss_start)}")
 
 
 if __name__ == "__main__":
